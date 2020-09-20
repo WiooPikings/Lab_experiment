db2f2f6fca7cbf6df1d2cb1a20b94a03e7601a2e
WiooPikings <71595254+WiooPikings@users.noreply.github.com>
2020-09-20 19:19:48 +0530

    Create Datatypes.py

---
+++ Experiment1/Datatypes.py
@@ -0,0 +1,4 @@
+## Integers and Floating Point
+
+'''In python, integers and numbers with decimal are two different data types.
+But in javascript, there is only one data type “number” to combine them.'''
