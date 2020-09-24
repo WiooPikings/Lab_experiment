4f9b383f87e6013d3c2b2c3c247db2ac6a3eb764
WiooPikings <71595254+WiooPikings@users.noreply.github.com>
2020-09-24 16:51:40 +0530

    Update Matplotlib.py

--- Expriment3/Matplotlib.py
+++ Expriment3/Matplotlib.py
@@ -7,110 +7,19 @@ import matplotlib.pyplot as plt
 plt.plot([1,2,3,4,10])
 #> [<matplotlib.lines.Line2D at 0x10edbab70>]
 
-# 'go' stands for green dots
-plt.plot([1,2,3,4,5], [1,2,3,4,10], 'go')
-plt.show()
-
-# Draw two sets of scatterplots in same plot
-
-# Draw two sets of points
-plt.plot([1,2,3,4,5], [1,2,3,4,10], 'go')  # green dots
-plt.plot([1,2,3,4,5], [2,3,4,5,11], 'b*')  # blue stars
-plt.show()
-
-#The plt object has corresponding methods to add each of this.
-plt.plot([1,2,3,4,5], [1,2,3,4,10], 'go', label='GreenDots')
-plt.plot([1,2,3,4,5], [2,3,4,5,11], 'b*', label='Bluestars')
-plt.title('A Simple Scatterplot')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend(loc='best')  # legend text comes from the plot's label parameter.
-plt.show()
-
-#The easy way to do it is by setting the figsize inside plt.figure() method.
-plt.figure(figsize=(10,7)) # 10 is width, 7 is height
-plt.plot([1,2,3,4,5], [1,2,3,4,10], 'go', label='GreenDots')  # green dots
-plt.plot([1,2,3,4,5], [2,3,4,5,11], 'b*', label='Bluestars')  # blue stars
-plt.title('A Simple Scatterplot')  
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.xlim(0, 6)
-plt.ylim(0, 12)
-plt.legend(loc='best')
-plt.show()
-
-# Draw 2 scatterplots in different panels
-
-# Create Figure and Subplots
-fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,4), sharey=True, dpi=120)
-# Plot
-ax1.plot([1,2,3,4,5], [1,2,3,4,10], 'go')  # greendots
-ax2.plot([1,2,3,4,5], [2,3,4,5,11], 'b*')  # bluestart
-# Title, X and Y labels, X and Y Lim
-ax1.set_title('Scatterplot Greendots'); ax2.set_title('Scatterplot Bluestars')
-ax1.set_xlabel('X');  ax2.set_xlabel('X')  # x label
-ax1.set_ylabel('Y');  ax2.set_ylabel('Y')  # y label
-ax1.set_xlim(0, 6) ;  ax2.set_xlim(0, 6)   # x axis limits
-ax1.set_ylim(0, 12);  ax2.set_ylim(0, 12)  # y axis limits
-# ax2.yaxis.set_ticks_position('none') 
-plt.tight_layout()
-plt.show()
-
-'''create one subplot at a time (using plt.subplot() or plt.add_subplot()) and immediately call plt.plot() or plt.{anything} 
-to modify that specific subplot (axes). Whatever method you call using plt will be drawn in the current axes'''
-
-plt.figure(figsize=(10,4), dpi=120) # 10 is width, 4 is height
-# Left hand side plot
-plt.subplot(1,2,1)  # (nRows, nColumns, axes number to plot)
-plt.plot([1,2,3,4,5], [1,2,3,4,10], 'go')  # green dots
-plt.title('Scatterplot Greendots')  
-plt.xlabel('X'); plt.ylabel('Y')
-plt.xlim(0, 6); plt.ylim(0, 12)
-# Right hand side plot
-plt.subplot(1,2,2)
-plt.plot([1,2,3,4,5], [2,3,4,5,11], 'b*')  # blue stars
-plt.title('Scatterplot Bluestars')  
-plt.xlabel('X'); plt.ylabel('Y')
-plt.xlim(0, 6); plt.ylim(0, 12)
-plt.show()
-
-# Draw multiple plots using for-loops using object oriented syntax
+# 1D Data
 import numpy as np
-from numpy.random import seed, randint
-seed(100)
-# Create Figure and Subplots
-fig, axes = plt.subplots(2,2, figsize=(10,6), sharex=True, sharey=True, dpi=120)
-# Define the colors and markers to use
-colors = {0:'g', 1:'b', 2:'r', 3:'y'}
-markers = {0:'o', 1:'x', 2:'*', 3:'p'}
-# Plot each axes
-for i, ax in enumerate(axes.ravel()):
-    ax.plot(sorted(randint(0,10,10)), sorted(randint(0,10,10)), marker=markers[i], color=colors[i])  
-    ax.set_title('Ax: ' + str(i))
-    ax.yaxis.set_ticks_position('none')
-plt.suptitle('Four Subplots in One Figure', verticalalignment='bottom', fontsize=16)    
-plt.tight_layout()
-plt.show()
+x = np.linspace(0, 10, 100)
+y = np.cos(x) 
+z = np.sin(x)
 
-# FuncFormatter using matplotlib
-from matplotlib.ticker import FuncFormatter
+#2D Data
+data = 2 * np.random.random((10, 10))
+data2 = 3 * np.random.random((10, 10))
+Y, X = np.mgrid[-3:3:100j, -3:3:100j]
+U = -1 - X**2 + Y
+V = 1 + X - Y**2
+from matplotlib.cbook import get_sample_data
+img = np.load(get_sample_data('axes_grid/bivariate_normal.npy'))
 
-def rad_to_degrees(x, pos):
-    'converts radians to degrees'
-    return round(x * 57.2985, 2)
-
-plt.figure(figsize=(12,7), dpi=100)
-X = np.linspace(0,2*np.pi,1000)
-plt.plot(X,np.sin(X))
-plt.plot(X,np.cos(X))
-# 1. Adjust x axis Ticks
-plt.xticks(ticks=np.arange(0, 440/57.2985, 90/57.2985), fontsize=12, rotation=30, ha='center', va='top')  # 1 radian = 57.2985 degrees
-# 2. Tick Parameters
-plt.tick_params(axis='both',bottom=True, top=True, left=True, right=True, direction='in', which='major', grid_color='blue')
-# 3. Format tick labels to convert radians to degrees
-formatter = FuncFormatter(rad_to_degrees)
-plt.gca().xaxis.set_major_formatter(formatter)
-plt.grid(linestyle='--', linewidth=0.5, alpha=0.15)
-plt.title('Sine and Cosine Waves\n(Notice the ticks are on all 4 sides pointing inwards, radians converted to degrees in x axis)', fontsize=14)
-plt.show()
 
